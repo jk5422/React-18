9a5b2d2fd6454c3353a3b4a555e50dbdf72f9bac
jaimin koladiya <koladiyajaimin@gmail.com>
2024-12-21 23:47:04 +0530

    change in title

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Notes App</h1>
+      <h1>Notes App in test branch</h1>
 
       {/* Add Note Section */}
       <div style={{ marginBottom: "20px" }}>
